7a7ebd1c9549f0b958e7cd52262240e49bdf9af5
Erick G-O <erick.gon.oli@gmail.com>
2022-09-07 00:24:20 -0300

    Exercicio Nome das Pedras Resolvido.

---
+++ 04/index.js
@@ -0,0 +1,18 @@
+const ladoA = 3;
+const ladoB = 2;
+
+//seu código aqui
+
+// Resolução utilizando o E(&&) Lógico.
+if (ladoA === ladoB && ladoA === 3) {
+    // Só imprimirá algo na tela se as duas condições acima forem true;
+    console.log("Terno");
+}
+
+// vResolução utilizando encadeamento do IF
+/*
+if (ladoA === ladoB) {
+    if (ladoA === 3) {
+        console.log("Terno");
+    }
+} */
