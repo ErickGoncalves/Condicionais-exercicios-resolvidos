996e0ba377a2c5a4a3bb51acdf55835d151f230c
Erick G-O <erick.gon.oli@gmail.com>
2022-09-07 00:47:42 -0300

    Exercicio Posicoes dos Jogadores Resolvido.

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,16 @@
-const alturaEmCm = 185;
+const alturaEmCm = 187;
 
 //seu código aqui
+
+// Resolução utilizando IF, Else IF e Else, para mais de uma condição.
+if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+    console.log("LÍBERO")
+} else if (alturaEmCm >= 186 && alturaEmCm <= 195) {
+    console.log("PONTEIRO")
+} else if (alturaEmCm >= 196 && alturaEmCm <= 205) {
+    console.log("OPOSTO")
+} else if (alturaEmCm > 205) {
+    console.log("CENTRAL")
+} else {
+    console.log("REPROVADO")
+}
