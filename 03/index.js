61a4acc0e9c53b2c8c1c6fc182238529c0f4695a
Erick G-O <erick.gon.oli@gmail.com>
2022-09-07 00:11:59 -0300

    Exercicio Dominó Resolvido.

--- 03/index.js
+++ 03/index.js
@@ -1,4 +1,16 @@
-const ladoA = 3;
-const ladoB = 3;
+const ladoA = 2;
+const ladoB = 2;
 
 //seu código aqui
+
+// Resolução utilizando Ternário.
+//const bucha = ladoA === ladoB ? "SIM" : "NÃO";
+//console.log(bucha);
+
+// Resolução utilizando o IF e Else.
+
+if (ladoA === ladoB) {
+    console.log("SIM")
+} else {
+    console.log("NÃO")
+}
