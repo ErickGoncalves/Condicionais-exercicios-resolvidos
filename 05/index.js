3e4676601f2a5700833dfcd64d35dec4a6823fc8
Erick G-O <erick.gon.oli@gmail.com>
2022-09-07 00:37:22 -0300

    Exercicio Peneira de Volei Resolvido.

--- 05/index.js
+++ 05/index.js
@@ -1,3 +1,15 @@
-const alturaEmCm = 185;
+const alturaEmCm = 125;
 
 //seu código aqui
+
+// Resolução utilizando ternário.
+// const peneira = alturaEmCm >= 180 ? "APROVADO" : "REPROVADO";
+// console.log(peneira);
+
+// Resolução utilizando IF e Else.
+
+if (alturaEmCm >= 180) {
+    console.log("APROVADO");
+} else {
+    console.log("REPROVADO")
+}
