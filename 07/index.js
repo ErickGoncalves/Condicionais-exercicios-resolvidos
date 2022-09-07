2f2c5c1bc90d4da986919fbddb2f9133be0d04da
Erick G-O <erick.gon.oli@gmail.com>
2022-09-07 00:57:43 -0300

    Exercicio Imposto de Renda Resolvido.

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,14 @@
-const aposentada = false;
+const aposentada = true;
 const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+
+// Resolução utilizando o ||(OU) lógico.
+if (aposentada || portadoraDeDoenca) {
+    console.log("ISENTA")
+} else if (totalDeRendimentos <= 2855970) {
+    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE")
+} else {
+    console.log("PEGA LEAO")
+}
