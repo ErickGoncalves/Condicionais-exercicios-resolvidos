adde7f413500f1af992cef3b28bb869e72382f61
Erick G-O <erick.gon.oli@gmail.com>
2022-09-07 01:07:29 -0300

    Exercicio Montanha Russa Resolvido.

--- 08/index.js
+++ 08/index.js
@@ -1,5 +1,14 @@
-const idade = 18;
-const possuiPatologia = false;
-const altura = 180;
+const idade = 12;
+const possuiPatologia = true;
+const altura = 155;
 const ehEstudante = false;
 
+//seu código aqui
+
+if (idade < 12 && idade > 65 || possuiPatologia || altura < 150) {
+    console.log("ACESSO NEGADO")
+} else if (ehEstudante || idade < 18) {
+    console.log("10 reais")
+} else {
+    console.log("20 reais")
+}
