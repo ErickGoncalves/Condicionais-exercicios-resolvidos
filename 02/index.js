2bb1973cfeb77a84c2e31565835e8a001c1764d8
Erick G-O <erick.gon.oli@gmail.com>
2022-09-07 00:04:56 -0300

    Exercicio Impar ou Par, Resolvido!

--- 02/index.js
+++ 02/index.js
@@ -1,4 +1,20 @@
-const jogada1 = 5;
-const jogada2 = 3;
+const jogada1 = 25;
+const jogada2 = 25;
+
+const soma = jogada1 + jogada2;
 
 //seu código aqui
+
+// resolução com ternario.
+/*
+
+const resultado = soma % 2 === 0 ? "par" : "impar";
+
+console.log(resultado);*/
+
+// resolução utilizando a condição IF e ELSE;
+if (soma % 2 === 0) {
+    console.log("par")
+} else {
+    console.log("impar")
+}
