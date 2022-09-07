e22165fac71bd245f0b8c72e5d77b952d44998a8
Erick G-O <erick.gon.oli@gmail.com>
2022-09-06 23:48:00 -0300

    Exercicio Pedra, Papel, Tesoura Resolvido.

--- 01/index.js
+++ 01/index.js
@@ -2,3 +2,29 @@ const jogada1 = "pedra"
 const jogada2 = "tesoura"
 
 //seu código aqui
+
+if (jogada1 === "pedra" && jogada2 === "tesoura") {
+    // primeira condição   
+    console.log("Jogador 1 venceu");
+} else if (jogada1 === "pedra" && jogada2 === "papel") {
+    // segunda condição   
+    console.log("Jogador 2 venceu");
+} else if (jogada1 === "tesoura" && jogada2 === "papel") {
+    // terceira condição   
+    console.log("Jogador 1 venceu");
+} else if (jogada1 === "tesoura" && jogada2 === "pedra") {
+    // quarta condição   
+    console.log("Jogador 2 venceu");
+} else if (jogada1 === "papel" && jogada2 === "pedra") {
+    // quinta condição   
+    console.log("Jogador 1 venceu");
+} else if (jogada1 === "papel" && jogada2 === "tesoura") {
+    // sexta condição   
+    console.log("Jogador 2 venceu");
+} else if (jogada1 === jogada2) {
+    //condição de empate
+    console.log("Empate");
+} else {
+    // condição inválida
+    console.log("Selecione uma das opções válidas para o jogo.")
+}
