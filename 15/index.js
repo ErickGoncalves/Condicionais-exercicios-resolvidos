1c6535186283ca7f31a4fc948a8378a9882f4b17
Erick G-O <erick.gon.oli@gmail.com>
2022-09-09 18:20:55 -0300

    Nome para Exibição, apenas com conceito truthiness

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,13 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+//seu código aqui
+
+if (apelido) {
+    console.log(apelido)
+} else if (sobrenome) {
+    console.log(primeiroNome + " " +
+        sobrenome)
+} else {
+    console.log(primeiroNome)
+}
