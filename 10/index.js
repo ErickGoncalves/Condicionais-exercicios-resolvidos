3559b17fc1f14a25be91c1b180ed89a77f735bd9
Erick G-O <erick.gon.oli@gmail.com>
2022-09-08 22:06:51 -0300

    Exercicio Vogais,consoantes e números Resolvido.

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,13 @@
-const caractere = "E";
+const caractere = "12";
+const vogaisMaiusculas = "A,E,I,O,U";
+const vogaisMinusculas = "a,e,i,o,u"
+
+if (vogaisMaiusculas.includes(caractere)) {
+    console.log(`${caractere} : É uma vogal maiúscula.`);
+} else if (vogaisMinusculas.includes(caractere)) {
+    console.log(`${caractere} : É uma vogal minúscula.`);
+} else if (caractere < 0 || caractere > 0) {
+    console.log(`${caractere} : É um número.`)
+} else {
+    console.log(`${caractere} : É uma consoante.`)
+}
