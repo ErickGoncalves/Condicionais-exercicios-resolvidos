dc490733dae3bd9b02bfbd6a688de6d7c01fb36e
Erick G-O <erick.gon.oli@gmail.com>
2022-09-09 18:18:42 -0300

    Controle de Consumo de Água Ingerida Resolvido

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,11 @@
 //quantidade de água ingerida em litro.
 const quantidadeDeAguaIngerida = 2;
 
+// As informações abaixo indicam o risco de complicações na saúde com base na injestão diária de água.
+if (quantidadeDeAguaIngerida < 1.5) {
+    console.log("Risco Alto - Você está ingerindo pouquissima água, beba mais água!")
+} else if (quantidadeDeAguaIngerida >= 1.5 && quantidadeDeAguaIngerida <= 3) {
+    console.log("Risco Moderado - Você está ingerindo pouca água, beba mais!")
+} else if (quantidadeDeAguaIngerida > 3) {
+    console.log("Risco Baixo - Você está ingerindo uma boa quantidade de água, parabéns!")
+}
