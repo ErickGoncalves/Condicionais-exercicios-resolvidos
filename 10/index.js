c7d31a76e00b68caab3406cc41fc79febdf95a33
Erick G-O <erick.gon.oli@gmail.com>
2022-09-09 22:09:12 -0300

    finalizado

--- 10/index.js
+++ 10/index.js
@@ -1,4 +1,4 @@
-const caractere = "12";
+const caractere = "0";
 const vogaisMaiusculas = "A,E,I,O,U";
 const vogaisMinusculas = "a,e,i,o,u"
 
@@ -6,7 +6,7 @@ if (vogaisMaiusculas.includes(caractere)) {
     console.log(`${caractere} : É uma vogal maiúscula.`);
 } else if (vogaisMinusculas.includes(caractere)) {
     console.log(`${caractere} : É uma vogal minúscula.`);
-} else if (caractere < 0 || caractere > 0) {
+} else if (caractere <= 0 || caractere >= 0) {
     console.log(`${caractere} : É um número.`)
 } else {
     console.log(`${caractere} : É uma consoante.`)
--- 17/index.js
+++ 17/index.js
@@ -5,4 +5,16 @@ const valorDoProduto = 100000;
 const quantidadeDoParcelamento = 10;
 
 //valor pago
-const valorPago = 300;
+const valorPago = 90000;
+
+if (valorDoProduto === valorPago) {
+    console.log("Produto quitado.")
+} else if (valorDoProduto > valorPago) {
+    let valorDaParcela = valorDoProduto / quantidadeDoParcelamento;
+
+    let quantidadeDeParcelasPagas = valorPago / valorDaParcela;
+
+    let final = quantidadeDoParcelamento - quantidadeDeParcelasPagas;
+
+    console.log(`Restam ${final} parcelas de R$ ${valorDaParcela}`)
+}
